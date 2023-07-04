b0b97909029eb65fb11742b1dcaa5bd136e264d3
zouyonghong <641261604@qq.com>
2023-07-04 10:58:11 +0800

    初始化插件

---
+++ dist/plugins/rhjc-web-main-plugin/index.js
@@ -0,0 +1,10 @@
+definePlugin('@plugins/rhjc-web-main-plugin', (function () { 'use strict';
+
+  var index = () => {
+    console.log("\u6D4B\u8BD5\u63D2\u4EF6\u52A0\u8F7D\u6210\u529F");
+  };
+
+  return index;
+
+}));
+//# sourceMappingURL=index.js.map
---
+++ plugins/rhjc-web-main-plugin/src/index.ts
@@ -0,0 +1,3 @@
+export default () => {
+  console.log('测试插件加载成功')
+}
